8f7016342f0e0e6cc0deadba46117d90c0448125
Jack Youstra <jyoustra@utexas.edu>
2021-05-19 15:58:54 -0500

    Thesis complete

--- data_processor.py
+++ data_processor.py
@@ -1,13 +1,25 @@
 from pathlib import Path
 import pandas as pd
 import re
+from tqdm import tqdm
 import xml.etree.ElementTree as et
 from math import ceil
 from scipy.stats import gmean
+import numpy as np
 
 # decodes filepaths into specification strings
 decoder = re.compile(r".*?(([a-zA-Z0-9]+)_s([0-9]+)_f([0-9]+)_ci([0-9]+)_co([0-9]+)).*?/solution1")
 figure_regex = re.compile(r"Slack \(MET\) :\s*([0-9.]*.{2})\s.*?Data Path Delay:\s*([0-9.]*.+?)\s.*?logic \s*([0-9.]*.+?)\s\(\s*([0-9.]*.+?)\)\s*route\s*([0-9.]*.+?)\s\(\s*([0-9.]*.+?)\).*?Logic\sLevels.*?([0-9]+).*?", re.DOTALL)
+core_power = re.compile(r".*conv_2d_0.*?([0-9.]+).*", re.DOTALL)
+lut_logic = re.compile(r".*LUT as Logic.*?([0-9.]+) \|.*", re.DOTALL)
+reg_and_shift = re.compile(r"Register.*?([0-9.]+) \|$", re.MULTILINE)
+ram = re.compile(r".*RAM.*?([0-9.]+) \|.*", re.DOTALL)
+tot_power = re.compile(r".*Total On-Chip Power.*?([0-9.]+)\s*\|.*", re.DOTALL)
+dynamic = re.compile(r"Dynamic.*?([0-9.]+)\s*\|", re.DOTALL)
+static = re.compile(r"Static.*?([0-9.]+)\s*\|", re.DOTALL)
+numparser = re.compile(r"([0-9.]+)")
+
+
 workingdir = Path("/u/jyoustra/scratch/hls_testbench")
 
 synth_projectdir = workingdir / "synthesized_projects"
@@ -28,13 +40,14 @@ def ns_denominate(text):
     else:
         assert(False, "Need to define the normalization")
 
+# signal (one axis), filter (one axis), channel in (one axis), channel out (one axis)
 resnet_dims = [
     (256, 2, 64, 64),
     (28, 1, 128, 512),
     (7, 3, 512, 512),
 ]
 
-for file in hlsdir.glob("*/solution1/syn/report/csynth.xml"):
+for file in tqdm(list(hlsdir.glob("*/solution1/syn/report/csynth.xml"))):
     name = str(file.absolute())
     grouper = decoder.match(name)
     if grouper:
@@ -70,6 +83,8 @@ for file in hlsdir.glob("*/solution1/syn/report/csynth.xml"):
         vivado_project = vivadodir / designation / f"{designation}.runs" / "impl_1"
         if vivado_project.exists():
             timingFile = vivado_project / "Zynq_Design_wrapper_timing_summary_routed.rpt"
+            powerFile = vivado_project / "Zynq_Design_wrapper_power_routed.rpt"
+            utilizationFile = vivado_project / "Zynq_Design_wrapper_utilization_placed.rpt"
             if timingFile.exists():
                 timingsDicts = []
                 with open(timingFile, "r") as timings:
@@ -119,16 +134,62 @@ for file in hlsdir.glob("*/solution1/syn/report/csynth.xml"):
                 sim_props.update(group_dict[0])
                 # print(sim_props)
                 # exit()
-
+            if powerFile.exists():
+                # get power of conv core
+                with open(powerFile, "r") as timings:
+                    text = timings.read()
+                    reg_shift = list(reg_and_shift.finditer(text))
+                    sim_props.update({
+                        "Total power" : float(tot_power.match(text).group(1)),
+                        "Dynamic total power" : float(dynamic.search(text).group(1)),
+                        "Static total power" : float(static.search(text).group(1)),
+                        "Core power" : float(core_power.match(text).group(1)),
+                        "LUT: logic util" : float(lut_logic.match(text).group(1)),
+                        "LUT: shift util" : float(reg_shift[1].group(1)),
+                        "Register util" : float(reg_shift[0].group(1)),
+                        "RAM util" : float(ram.match(text).group(1)),
+                    })
+            if utilizationFile.exists():
+                with open(utilizationFile, "r") as util:
+                    text = util.read()
+                    end = text.find("1. Slice Logic\n------")
+                    # read in all the percentages
+                    lastnums = [z[-1].group() for z in [list(numparser.finditer(x)) for x in text[end:].splitlines()[:18]] if len(z) > 0]
+                    percentages = [float(x) for x in lastnums if '.' in x[:-1]]
+                    sim_props.update({
+                        "area_pct" : np.mean(percentages)
+                    })
 
         # find number of times have to compute
+        # divide by 1b to get to s
+        time_factor = 1000 * 1000 * 1000
         for idx, vals in enumerate(resnet_dims):
             s, f, ci, co = vals
             num_calls = int(ceil(ci / chan_in)) * int(ceil(co / chan_out)) * (int(ceil(f / filt)) ** 2) * (int(ceil(s / signal)) ** 2)
             sim_props.update({
                 "Res {}: Itercount".format(idx) : num_calls,
-                "Res {}: Synth Area-Delay".format(idx) : num_calls * lut * synth_delay
+                "Res {}: Delay (s)".format(idx) : num_calls * synth_delay / time_factor,
+                "Res {}: Synth Area-Delay (s * LUT)".format(idx) : num_calls * lut * synth_delay / time_factor,
             })
+            if "Total power" in sim_props and "max datapath delay" in sim_props and "area_pct" in sim_props:
+                for power_metric in ["Total power", "Core power"]:
+                    worst_EDP = sim_props[power_metric] * num_calls * (sim_props["max datapath delay"] + max(0.0, sim_props["max slack"])) / time_factor
+                    best_EDP = sim_props[power_metric] * num_calls * (sim_props["min datapath delay"] + max(0.0, sim_props["min slack"])) / time_factor
+                    sim_props.update({
+                        f"Res {idx}: Worst-case {power_metric.lower()} usage (W*s) (board min freq)" : worst_EDP,
+                        f"Res {idx}: Worst-case {power_metric.lower()} usage (W*s) (chip min freq)" : sim_props[power_metric] * num_calls  * sim_props["max datapath delay"] / time_factor,
+                        f"Res {idx}: Best-case {power_metric.lower()} usage (W*s) (board min freq)" : best_EDP,
+                        f"Res {idx}: Best-case {power_metric.lower()} usage (W*s) (chip min freq)" : sim_props[power_metric] * num_calls  * sim_props["min datapath delay"] / time_factor,
+                        f"Res {idx}: Total EADP ({power_metric}) (W*s*chip area used) (Worst-case)" : worst_EDP * sim_props["area_pct"],
+                        f"Res {idx}: Total EADP ({power_metric}) (W*s*chip area used) (Best-case)" : best_EDP * sim_props["area_pct"],
+                    })
+        # find averages
+        # lsit b/c modifying inplace
+        for key, sim_stat in list(sim_props.items()):
+            components = key.split(":")
+            if len(components) == 2 and "Res 0" in components[0]:
+                new_key = f"Resnet (average):{components[1]}"
+                sim_props[new_key] = sim_props.get(new_key, 0.0) + (sim_stat / len(resnet_dims))
 
         dictlist.append(sim_props)
     else:
--- data_visualize.py
+++ data_visualize.py
@@ -1,26 +1,189 @@
 import pandas as pd
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
+from scipy.stats.stats import gmean
+from pathlib import Path
+import numpy as np
+from sklearn.preprocessing import normalize
+import matplotlib.ticker as mtick
 
 df = pd.read_csv("results.csv")
+# only select valid results
+df.dropna(inplace=True)
 
-elements = df.loc[(df['chan_in'] == 1) & (df['chan_out'] == 1) & (df['signal'] == 16) & (df['accelerator'] != 'nvdla')]
+def EADP_chart(key):
+    path = Path(f"figs/Best PADP for {key} size.png")
+    if not path.exists() or True:
+        name = next(x for x in reversed(df.columns) if 'Total EADP' in x and 'Worst' in x)
+        filters = df.pivot_table(values=name, index=key, columns='accelerator', aggfunc=min)
+        filters.plot(legend=True, xticks=filters.index, logy=True, title=f"PADP vs {key} size (lower is better)", xlabel=f"{key} size".capitalize(), ylabel="Log PADP (W * s * % area utilization)")
+        plt.savefig(path, bbox_inches='tight')
+        plt.clf()
+EADP_chart('filter')
+EADP_chart('signal')
+EADP_chart('Core power')
 
-sorts = elements.sort_values("filter")
-#print(sorts)
+def stats():
+    for desc in ['Worst', 'Best']:
+        name = next(x for x in reversed(df.columns) if 'Total EADP' in x and desc in x)
+        filters = df.pivot_table(values=name, columns='accelerator', aggfunc=min)
+        f = filters.div(filters.min(axis=1), axis=0)
+        print(f)
+stats()
 
-for keyword in [x for x in sorts.columns if 'Area-Delay' in x]:
-    # vary filters over the popular two
-    for accelerator in sorts["accelerator"].unique():
-        accel_cols = sorts.loc[sorts['accelerator'] == accelerator]
-        xval = accel_cols['filter'].values[1:]
-        y = accel_cols[keyword].values[1:]
-        x = list(range(len(xval)))
-        plt.plot(x, y, label=accelerator)
-        plt.xticks(x, xval)
-    plt.xlabel("Filter size")
-    plt.ylabel(keyword + "(steady-state ns delay * LUTs * times called)")
-    plt.title("EDP for entire ResNet layer (16x16 signal tile, lower is better)")
-    plt.legend()
-    plt.savefig('figs/{}.png'.format(keyword))
+path = Path(f"figs/accel_counts.png")
+if not path.exists():
+    one_chan = df.loc[(df['chan_in'] == 1) & (df['chan_out'] == 1)]
+    multi_chan = df.loc[(df['chan_in'] != 1) | (df['chan_out'] != 1)]
+    single_counts = pd.value_counts(one_chan['accelerator'])
+    total_counts = pd.value_counts(multi_chan['accelerator'])
+    resulting = pd.concat([single_counts, total_counts], axis=1)
+    resulting.fillna(0, inplace=True)
+    resulting.columns = ["Monochannel", "Multichannel"]
+    print(resulting)
+    resulting.plot.bar(stacked=True, title="Evaluated configurations by accelerator", ylabel="Number of evaluated configurations", rot=0)
+    plt.savefig(path, bbox_inches='tight')
     plt.clf()
 
+def core_eng_chart():
+    non_core_energy = (df["Total power"] - df["Core power"]).values
+    df["non core energy"] = non_core_energy
+    figpath = Path('figs/core_noncore_energy.png')
+    if not figpath.exists():
+        #pct_run = df.assign(non_core_energy=(df["Total power"] - df["Core power"]).values)
+        x = [gmean(non_core_energy), gmean(df["Core power"].values)]
+        labels = ["Non-Core Energy", "Convolution Core Energy"]
+        # add max potential
+        # maximum_observed = max(df["Core power"].values)
+        # x[0] -= maximum_observed
+        # x.append(maximum_observed)
+        # labels.append("Maximum observed core energy")
+
+        plt.pie(x, labels=labels, autopct='%1.1f%%')
+        plt.legend()
+        plt.title("Non-Core energy vs convolution core energy")
+        # Caption: geometric mean over best synthesized configurations
+        plt.savefig(figpath, bbox_inches='tight')
+        plt.clf()
+    figpath = Path('figs/top_core_noncore_energy.png')
+    if not figpath.exists():
+        accelerators = df["accelerator"].unique()
+        core = []
+        noncore = []
+        for accel in accelerators:
+            accel_df = df.loc[df["accelerator"] == accel]
+            best_idx = np.argmin(accel_df["Resnet (average): Total EADP (Core power) (W*s*chip area used) (Worst-case)"].values)
+            core.append(accel_df["Core power"].values[best_idx])
+            noncore.append(accel_df["non core energy"].values[best_idx])
+        x = [gmean(noncore), gmean(core)]
+        labels = ["Non-Core Energy", "Convolution Core Energy"]
+        plt.pie(x, labels=labels, autopct='%1.1f%%')
+        plt.legend()
+        plt.title("Non-Core energy vs convolution core energy (best configuration)")
+        # Caption: geometric mean over best synthesized configurations
+        plt.savefig(figpath, bbox_inches='tight')
+        plt.clf()
+    figpath = Path('figs/max_observed_core.png')
+    if not figpath.exists():
+        #pct_run = df.assign(non_core_energy=(df["Total power"] - df["Core power"]).values)
+        idx = np.argmax(df["Core power"].values / non_core_energy)
+        x = [non_core_energy[idx], df["Core power"].values[idx]]
+        labels = ["Non-Core Energy", "Convolution Core Energy"]
+        # add max potential
+        # maximum_observed = max(df["Core power"].values)
+        # x[0] -= maximum_observed
+        # x.append(maximum_observed)
+        # labels.append("Maximum observed core energy")
+
+        plt.pie(x, labels=labels, autopct='%1.1f%%')
+        plt.legend()
+        plt.title("Non-Core energy vs convolution core energy (worst case synthesis)")
+        # Caption: geometric mean over best synthesized configurations
+        plt.savefig(figpath, bbox_inches='tight')
+        plt.clf()
+
+core_eng_chart()
+
+def latency_chart():
+    figpath = Path('figs/static_dynamic.png')
+    if not figpath.exists():
+        #pct_run = df.assign(non_core_energy=(df["Total power"] - df["Core power"]).values)
+        x = [gmean(df["Static total power"].values), gmean(df["Dynamic total power"].values)]
+        labels = ["Static total power", "Dynamic total power"]
+        # add max potential
+        # maximum_observed = max(df["Core power"].values)
+        # x[0] -= maximum_observed
+        # x.append(maximum_observed)
+        # labels.append("Maximum observed core energy")
+
+        plt.pie(x, labels=labels, autopct='%1.1f%%')
+        plt.legend()
+        plt.title("Share of power: Dynamic vs Static")
+        # Caption: geometric mean over best synthesized configurations
+        plt.savefig(figpath, bbox_inches='tight')
+        plt.clf()
+    figpath = Path('figs/top_static_dynamic.png')
+    if not figpath.exists():
+        accelerators = df["accelerator"].unique()
+        core = []
+        noncore = []
+        for accel in accelerators:
+            accel_df = df.loc[df["accelerator"] == accel]
+            best_idx = np.argmin(accel_df["Resnet (average): Total EADP (Core power) (W*s*chip area used) (Worst-case)"].values)
+            core.append(accel_df["Static total power"].values[best_idx])
+            noncore.append(accel_df["Dynamic total power"].values[best_idx])
+        x = [gmean(noncore), gmean(core)]
+        labels = ["Static total power", "Dynamic total power"]
+        plt.pie(x, labels=labels, autopct='%1.1f%%')
+        plt.legend()
+        plt.title("Share of power: Dynamic vs Static (best configuration)")
+        # Caption: geometric mean over best synthesized configurations
+        plt.savefig(figpath, bbox_inches='tight')
+        plt.clf()
+
+latency_chart()
+
+def delay_chart():
+    for label in ["max", "min"]:
+        figpath = Path(f'figs/{label}_delay_sources.png')
+        if not figpath.exists():
+            #pct_run = df.assign(non_core_energy=(df["Total power"] - df["Core power"]).values)
+            x = [gmean(df[f"{label} route datapath delay"].values), gmean(df[f"{label} logic datapath delay"].values)]
+            labels = [f"{label} route".capitalize(), f"{label} logic".capitalize()]
+            # add max potential
+            # maximum_observed = max(df["Core power"].values)
+            # x[0] -= maximum_observed
+            # x.append(maximum_observed)
+            # labels.append("Maximum observed core energy")
+
+            plt.pie(x, labels=labels, autopct='%1.1f%%', normalize=True)
+            plt.title(f"{label} route delay vs {label} logic delay".capitalize())
+            # Caption: geometric mean over best synthesized configurations
+            plt.savefig(figpath, bbox_inches='tight')
+            plt.clf()
+    figpath = Path(f'figs/delay_sources.png')
+    if not figpath.exists():
+        x = ["Worst case", "Best case"]
+        y = []
+        labels = []
+        for label in ["max", "min"]:
+            #pct_run = df.assign(non_core_energy=(df["Total power"] - df["Core power"]).values)
+            y.append([gmean(df[f"{label} route datapath delay"].values), gmean(df[f"{label} logic datapath delay"].values)])
+            labels.append([f"{label} route".capitalize(), f"{label} logic".capitalize()])
+        y = np.array(y).transpose()
+        y = normalize(y, axis=0, norm='l1')
+        print(y)
+
+        plt.bar(x, y[0], label="Route delay")
+        plt.bar(x, y[1], label="Datapath delay", bottom=y[0])
+        plt.gca().get_yaxis().set_major_formatter(mtick.PercentFormatter(1.0))
+        #plt.xlabel(["Worst-case delay", "Best-case delay"])
+        plt.ylabel("Percentage of total delay")
+        plt.title(f"Route delay vs logic delay".capitalize())
+        plt.legend()
+        # Caption: geometric mean over best synthesized configurations
+        plt.savefig(figpath, bbox_inches='tight')
+        plt.clf()
+    
+delay_chart()
